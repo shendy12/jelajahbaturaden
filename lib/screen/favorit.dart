8e36ee716d84e44b580e1df674414203a324738b
bagusrm <dwirakhmawan@gmail.com>
2025-06-20 09:52:55 +0700

    Halaman daftar favorit user Fixes #11

--- lib/screen/favorit.dart
+++ lib/screen/favorit.dart
@@ -1,14 +1,84 @@
 import 'package:flutter/material.dart';
-class Favorit extends StatefulWidget {
-  const Favorit({super.key});
+import 'package:http/http.dart' as http;
+
+class FavoritePage extends StatefulWidget {
+  const FavoritePage({Key? key}) : super(key: key);
 
   @override
-  State<Favorit> createState() => _FavoritState();
+  State<FavoritePage> createState() => _FavoritePageState();
 }
 
-class _FavoritState extends State<Favorit> {
+class _FavoritePageState extends State<FavoritePage> {
+  late Future<List<WisataFavorite>> favorites;
+
+  @override
+  void initState() {
+    super.initState();
+    favorites = fetchFavorites();
+  }
+
+  Future<List<WisataFavorite>> fetchFavorites() async {
+    final response = await http.get(Uri.parse('http://yourapi.com/api/favorite/getUserFavorites'));
+
+    if (response.statusCode == 200) {
+      final List data = json.decode(response.body);
+      return data.map((e) => WisataFavorite.fromJson(e)).toList();
+    } else {
+      throw Exception("Gagal memuat data favorit");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("Favorit Saya")),
+      body: FutureBuilder<List<WisataFavorite>>(
+        future: favorites,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final items = snapshot.data!;
+            return GridView.builder(
+              padding: EdgeInsets.all(16),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisExtent: 220,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+              ),
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final wisata = items[index];
+                return Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade200,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.memory(
+                          wisata.foto,
+                          height: 120,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(wisata.namawisata, style: TextStyle(fontWeight: FontWeight.bold)),
+                      Text(wisata.namakategori),
+                    ],
+                  ),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Center(child: Text("Gagal memuat data"));
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
   }
 }
