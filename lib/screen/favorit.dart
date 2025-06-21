e79ee67541eea05f546ddfd03e773ad96f1b2d2e
bagusrm <dwirakhmawan@gmail.com>
2025-06-21 15:35:03 +0700

    lanjutan halaman favorit Fixes #36

--- lib/screen/favorit.dart
+++ lib/screen/favorit.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:jelajahbaturaden/konstanta.dart';
+import 'package:jelajahbaturaden/model/favoritmodel.dart';
 
 class FavoritePage extends StatefulWidget {
   const FavoritePage({Key? key}) : super(key: key);
